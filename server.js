b1f077409c7d8cbea4630b1a66fb8f4cfff1b6d6
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2022-11-17 23:09:06 +0530

    node basics started

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  console.log("Server called");
+  res.setHeader('Content-Type', 'text/html');
+  console.log(req.url);
+  let path = "./views";
+  switch (req.url) {
+    case '/':
+      path += '/index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += '/about.html';
+      res.statusCode = 200;
+      break;
+    case '/aboutus':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += '/404.html';
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (err, file) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(file);
+      res.end("Finished!!");
+    }
+  })
+})
+server.listen(3000, 'localhost', () => {
+  console.log("Server listen");
+})
