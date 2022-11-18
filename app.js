6156843e254700414e548f9e70866f603ee00f0e
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2022-11-18 19:57:05 +0530

    express basics started

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World 123')
+})
+
+app.get('/about', function (req, res) {
+  res.sendFile('./views/about.html', { root: __dirname });
+  console.log(__dirname);
+})
+
+//redirect
+app.get('/aboutus', (req, res) => {
+  res.redirect('./about');
+})
+
+//middleware
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname }) //middleware
+})
+
+app.listen(3000, () => {
+  console.log("server started");
+})
