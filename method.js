3673d327a559e2c371550c906e507838362b2697
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2022-11-20 11:27:28 +0530

    learn basic api methods

--- method.js
+++ method.js
@@ -1,15 +1,52 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-let user = {};
-app.get('/user', (req, res) => {
-  res.send(user);
+
+let user = [
+  {
+    id: 1,
+    name: 'abhishek',
+    age: 40
+  },
+  {
+    id: 2,
+    name: 'shyam',
+    age: 30
+  },
+  {
+    id: 3,
+    name: 'nikhil',
+    age: 20
+  }
+];
+
+
+//query
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  let { name, age } = req.query;
+  let isDataSend = false;
+  let filteredUsers = [];
+  if (typeof name == 'string' && name.length > 0) {
+    filteredUsers = user.filter((user) => user.name.toLocaleLowerCase() == name.toLocaleLowerCase());
+    isDataSend = true;
+  }
+  if (!!age && !isNaN(age) && Number(age) >= 0) {
+    filteredUsers = filteredUsers.filter((user) => user.age >= age); // use single & operater 
+    isDataSend = true;
+  }
+  res.send(isDataSend ? filteredUsers : user);
 })
+
+// app.get('/user', (req, res) => {
+//   res.send(user);
+// })
 
 app.post('/user', (req, res) => {
   console.log(req.body);
   //then i can put this in db 
-  user = req.body;
+  let userObj = req.body;
+  user = [userObj];
   res.json({
     message: "Data received successfully",
     user: req.body
@@ -18,20 +55,29 @@ app.post('/user', (req, res) => {
 
 app.patch("/user", (req, res) => {
   let dataToBeStored = req.body;
+  const userObj = {};
   for (const key in dataToBeStored) {
-    user[key] = dataToBeStored[key];
+    userObj[key] = dataToBeStored[key];
   }
+  user.push(userObj);
   res.json({
     message: "Data updated"
   })
 })
 
 app.delete("/user", (req, res) => {
-  users = {},
+  user = {},
     res.json({
       message: "user deleted"
     });
 })
 
+//params
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  let userData = user.find((data) => data.id == req.params.id);
+  res.json({ message: "user id called", id: req.params, userData });
+  // res.send("Id user called");
+})
 
 app.listen(5000);
