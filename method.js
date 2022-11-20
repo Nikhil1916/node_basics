7e321c182f199d98fd08250fc257eb6346a626aa
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2022-11-20 12:32:31 +0530

    added mounting

--- method.js
+++ method.js
@@ -20,14 +20,87 @@ let user = [
   }
 ];
 
+const useRouter = express.Router();
+app.use("/user", useRouter);
+useRouter
+  .route("/")
+  .get(getUser)
+  .post(addUser)
+  .delete(deleteUsers)
+
+useRouter
+  .route("/:id")
+  .get(getUserById)
+  .patch(updateUser)
+  .delete(deleteUserById)
+
 
 //query
-app.get("/user", (req, res) => {
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+//   let { name, age } = req.query;
+//   let isDataSend = false;
+//   let filteredUsers = [];
+//   if (typeof name == 'string' && name.length > 0) {
+//     filteredUsers = user.filter((user) => user.name.toLocaleLowerCase() == name.toLocaleLowerCase());
+//     isDataSend = true;
+//   }
+//   if (!!age && !isNaN(age) && Number(age) >= 0) {
+//     filteredUsers = filteredUsers.filter((user) => user.age >= age); // use single & operater 
+//     isDataSend = true;
+//   }
+//   res.send(isDataSend ? filteredUsers : user);
+// })
+
+// app.get('/user', (req, res) => {
+//   res.send(user);
+// })
+
+// app.post('/user', (req, res) => {
+//   console.log(req.body);
+//   //then i can put this in db 
+//   let userObj = req.body;
+//   user = [userObj];
+//   res.json({
+//     message: "Data received successfully",
+//     user: req.body
+//   });
+// });
+
+// app.patch("/user", (req, res) => {
+//   let dataToBeStored = req.body;
+//   const userObj = {};
+//   for (const key in dataToBeStored) {
+//     userObj[key] = dataToBeStored[key];
+//   }
+//   user.push(userObj);
+//   res.json({
+//     message: "Data updated"
+//   })
+// })
+
+// app.delete("/user", (req, res) => {
+//   user = {},
+//     res.json({
+//       message: "user deleted"
+//     });
+// })
+
+//params
+// app.get("/user/:id", (req, res) => {
+//   console.log(req.params);
+//   let userData = user.find((data) => data.id == req.params.id);
+//   res.json({ message: "user id called", id: req.params, userData });
+//   // res.send("Id user called");
+// })
+
+function getUser(req, res) {
   console.log(req.query);
   let { name, age } = req.query;
   let isDataSend = false;
   let filteredUsers = [];
   if (typeof name == 'string' && name.length > 0) {
+    // filteredUsers = user.filter((user) => user.name.toLocaleLowerCase() == name.toLocaleLowerCase());
     filteredUsers = user.filter((user) => user.name.toLocaleLowerCase() == name.toLocaleLowerCase());
     isDataSend = true;
   }
@@ -36,48 +109,62 @@ app.get("/user", (req, res) => {
     isDataSend = true;
   }
   res.send(isDataSend ? filteredUsers : user);
-})
+}
 
-// app.get('/user', (req, res) => {
-//   res.send(user);
-// })
+function updateUser(req, res) {
+  let dataToBeStored = req.body;
+  const id = req.params.id;
+  let index = null;
+  user.forEach((user, i) => {
+    if (user.id == id) {
+      index = i
+    }
+  });
+  const userObj = user[index];
+  for (const key in dataToBeStored) {
+    userObj[key] = dataToBeStored[key];
+  }
+  user[index] = userObj
+  res.json({
+    message: "Data updated"
+  })
+}
 
-app.post('/user', (req, res) => {
+function addUser(req, res) {
   console.log(req.body);
   //then i can put this in db 
   let userObj = req.body;
-  user = [userObj];
+  let addUser = {};
+  for (const key in userObj) {
+    addUser[key] = userObj[key]
+  }
+  addUser['id'] = user.length + 1;
+  user.push(addUser);
   res.json({
     message: "Data received successfully",
     user: req.body
   });
-});
+}
 
-app.patch("/user", (req, res) => {
-  let dataToBeStored = req.body;
-  const userObj = {};
-  for (const key in dataToBeStored) {
-    userObj[key] = dataToBeStored[key];
-  }
-  user.push(userObj);
-  res.json({
-    message: "Data updated"
-  })
-})
-
-app.delete("/user", (req, res) => {
+function deleteUsers(req, res) {
   user = {},
     res.json({
       message: "user deleted"
     });
-})
+}
 
-//params
-app.get("/user/:id", (req, res) => {
+function deleteUserById(req, res) {
+  console.log(req.params);
+  user.splice(req.params.name, 1),
+    res.json({
+      message: "user deleted"
+    });
+}
+
+function getUserById(req, res) {
   console.log(req.params);
   let userData = user.find((data) => data.id == req.params.id);
   res.json({ message: "user id called", id: req.params, userData });
-  // res.send("Id user called");
-})
+}
 
 app.listen(5000);
